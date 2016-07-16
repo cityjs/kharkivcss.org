7939e6aef09cc31ec3d4936b5ba767a26959895a
NightNei <nei.css@gmail.com>
2016-07-16 12:34:45 +0300

    markup

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(function(){
+	function initialize() {
+		var mapCanvas = document.getElementById('mapCanvas');
+		var myLatlng = new google.maps.LatLng(49.98935276, 36.22232348);
+		var mapOptions = {
+			center: myLatlng,
+			zoom: 15,
+			mapTypeControl: false,
+			draggable: true,
+			scaleControl: false,
+			scrollwheel: false,
+			navigationControl: false,
+			streetViewControl: true,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		var map = new google.maps.Map(mapCanvas, mapOptions);
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			title: 'Fabrika.space',
+		});
+	}
+	google.maps.event.addDomListener(window, 'load', initialize);
+});
+
+$(window).on('load', function(){
+	if($(window).width() < 768) return;
+
+	$('.mainMenu ul li a').on('click', function(e){
+		var hash = e.originalEvent.currentTarget.hash;
+		if(!hash) return;
+
+		e.originalEvent.preventDefault();
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top - $('.rowHeader').height() - 20
+		}, 600, 'linear');
+	});
+
+
+	var menuItems = $('.mainMenu').find('a[href^="#"]').map(function(i, a){
+		var href = a.getAttribute('href');
+		return {
+			id: href,
+			top: $(href).offset().top - $('.rowHeader').height() - 21
+		};
+	});
+	menuItems = [].slice.call(menuItems).reverse();
+
+	$(window).on('scroll', function(){
+		$('.mainMenu li').removeClass('act');
+		var item;
+		menuItems.forEach(function(n){ !item && n.top < $(window).scrollTop() && (item = n) });
+		item && $('.mainMenu').find('a[href="'+item.id+'"]').parent().addClass('act');
+	});
+});
